347786424a61f5bce8a2f1ead8d36cfed8571516
AbuZaed98 <abuzaed1998@gmail.com>
2020-11-24 17:08:02 +0200

    first commit

---
+++ app/src/main/java/com/example/project_lab/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.project_lab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/project_lab/Register.java
@@ -0,0 +1,15 @@
+package com.example.project_lab;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+    }
+}
